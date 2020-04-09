0c33675aa67049c1ea128a2ed2d7258cfdff4b0e
Jason Barry <barryofguilder@gmail.com>
2020-04-08 21:46:07 -0400

    Update meat bundles

--- seeders/20200226025714-meat-bundle.js
+++ seeders/20200226025714-meat-bundle.js
@@ -7,10 +7,11 @@ export default {
         price: '$68',
         featured: true,
         items: `
-          5lb. Ground Chuck,
-          5lb. Bonesless Chicken Breasts,
-          5lb. Boston Butt Pork Roast,
-          5lb. Bonelesss Pork Chops
+          5 lbs. Ground Chuck,
+          2 ½ lbs. Bonesless Chicken Breasts,
+          2 ½ lbs. Boneless Chicken Tenders,
+          5 lbs. Boston Butt Pork Roast,
+          5 lbs. Bonelesss Pork Chops
         `,
       },
       {
@@ -23,7 +24,8 @@ export default {
           3 ½ lbs. Boneless Pork Chops,
           4 lbs. Country Style Pork Ribs,
           2 ½ lbs. Market Style Bacon,
-          5 lbs. Boneless Chicken Breasts,
+          2 ½ lbs. Boneless Chicken Breasts,
+          2 ½ lbs. Boneless Chicken Tenders,
           10 lbs. Chicken Leg Quarters
         `,
       },
@@ -47,11 +49,12 @@ export default {
         price: '$129',
         featured: true,
         items: `
-          10lb. Fresh Ground Chuck,
-          10lb. Boneless Pork Chops,
-          10lb. Country Style Pork Ribs,
-          10lb. Boneless Chicken Breasts,
-          10lb. Chicken Leg Quarters
+          10 lbs. Fresh Ground Chuck,
+          10 lbs. Boneless Pork Chops,
+          10 lbs. Country Style Pork Ribs,
+          5 lbs. Boneless Chicken Breasts,
+          5 lbs. Boneless Chicken Tenders,
+          10 lbs. Chicken Leg Quarters
         `,
       },
       {
@@ -71,7 +74,8 @@ export default {
           4 lbs. Country Style Pork Ribs,
           3 lbs. Pork Sausage,
           2 ½ lbs. Sliced Smoked Bacon,
-          10 lbs. Boneless Chicken Breasts,
+          5 lbs. Boneless Chicken Breasts,
+          5 lbs. Boneless Chicken Tenders,
           6 lbs. Bone-In Chicken Breasts,
           10 lbs. Chicken Leg Quarters,
           5 lbs. Chicken Wings
@@ -90,7 +94,8 @@ export default {
           10 lbs. Boston Butt Pork Roast,
           10 lbs. Pork Sausage,
           10 lbs. Sliced Smoked Bacon,
-          10 lbs. Boneless Chicken Breasts,
+          5 lbs. Boneless Chicken Breasts,
+          5 lbs. Boneless Chicken Tenders,
           10 lbs. Chicken Leg Quarters
         `,
       },
@@ -116,7 +121,7 @@ export default {
           4 (7oz) Beef N.Y. Strip Steaks,
           2 lbs. Beef Stew,
           5 lbs. Beef Ground Chuck,
-          2 ½ lbs. Boneless Chicken Breast,
+          2 ½ lbs. Boneless Chicken Tenders,
           2 ½ lbs. Boneless Pork Chops
         `,
       },
@@ -128,13 +133,13 @@ export default {
         items: `
           2 (7 oz.) Ribeye Steaks,
           2 (7 oz.) N.Y. Strip Steaks,
-          2lb. Beef Chuck Roast,
-          2lb. Beef Cube Steak,
-          5lb. Beef Ground Chuck,
-          5lb. Boneless Pork Chops,
-          3lb. Pork Sausage,
-          2 ½ lb. Sliced Slab Bacon,
-          10lb. Bone-in Chicken Breast
+          2 lbs. Beef Chuck Roast,
+          2 lbs. Beef Cube Steak,
+          5 lbs. Beef Ground Chuck,
+          5 lbs. Boneless Pork Chops,
+          3 lbs. Pork Sausage,
+          2 ½ lbs. Sliced Slab Bacon,
+          10 lbs. Bone-in Chicken Breast
         `,
       },
     ]);
