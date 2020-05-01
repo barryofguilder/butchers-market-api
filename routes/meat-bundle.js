a9e1daaa78df21a2a5aed8250f0b8044a18a050b
Jason Barry <barryofguilder@gmail.com>
2020-05-01 15:04:33 -0400

    Can edit meat bundles

--- routes/meat-bundle.js
+++ routes/meat-bundle.js
@@ -18,4 +18,25 @@ router.get('/', async ctx => {
   ctx.body = ctx.app.serialize('meat-bundle', meatBundles);
 });
 
+router.get('/:id', async ctx => {
+  const id = ctx.params.id;
+  const meatBundle = await ctx.app.db.MeatBundle.findOrFail(id);
+
+  ctx.body = ctx.app.serialize('meat-bundle', meatBundle);
+});
+
+router.patch('/:id', async ctx => {
+  const id = ctx.params.id;
+  const attrs = ctx.request.body.data.attributes;
+
+  attrs.items = attrs.items ? attrs.items.join(',') : attrs.items;
+
+  const meatBundle = await ctx.app.db.MeatBundle.findOrFail(id);
+
+  meatBundle.set(attrs);
+  await meatBundle.save();
+
+  ctx.body = ctx.app.serialize('meat-bundle', meatBundle);
+});
+
 export default router.routes();
