13526cae883ca20a6cd5b357ae2434af0bf250c6
Jason Barry <jbarry@hey.com>
2022-12-03 17:20:26 -0500

    Start working to make it use Postgres

--- config/db.js
+++ config/db.js
@@ -3,12 +3,30 @@ dotenv.config();
 
 export default {
   development: {
-    dialect: 'sqlite',
-    storage: process.env.DB_STORAGE,
+    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
   },
   test: {},
   production: {
-    dialect: 'sqlite',
-    storage: process.env.DB_STORAGE,
+    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
   },
 };
+
+// export default {
+//   development: {
+//     dialect: 'sqlite',
+//     storage: process.env.DB_STORAGE,
+//   },
+//   test: {},
+//   production: {
+//     dialect: 'sqlite',
+//     storage: process.env.DB_STORAGE,
+//   },
+// };
