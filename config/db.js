bd563a3b494f9070f8dc4ef7562e3a41eca5a6bb
Jason Barry <jbarry@hey.com>
2023-03-02 17:56:04 -0500

    Update db and env for Render

--- config/db.js
+++ config/db.js
@@ -4,17 +4,17 @@ dotenv.config();
 export default {
   development: {
     dialect: 'postgres',
-    host: process.env.RDS_HOSTNAME,
-    username: process.env.RDS_USERNAME,
-    password: process.env.RDS_PASSWORD,
-    database: process.env.RDS_DB_NAME,
+    url: process.env.DB_URL,
+    dialectOptions: {
+      ssl: true,
+    },
   },
   test: {},
   production: {
     dialect: 'postgres',
-    host: process.env.RDS_HOSTNAME,
-    username: process.env.RDS_USERNAME,
-    password: process.env.RDS_PASSWORD,
-    database: process.env.RDS_DB_NAME,
+    url: process.env.DB_URL,
+    dialectOptions: {
+      ssl: true,
+    },
   },
 };
