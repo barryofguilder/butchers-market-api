6bfe4f837df293cd1ba92cac69b73cebdc2e6fc6
Jason Barry <jbarry@hey.com>
2023-03-02 23:59:28 -0500

    Cleanup old deployment

--- backup_db.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-_now=$(date +"%Y_%m_%d")
-_file="butcher_$_now.bak"
-sqlite3 ~/public_html/db/butcher.sqlite3 .dump > ~/public_html/db_backups/$_file
--- config/db.js
+++ config/db.js
@@ -10,17 +10,11 @@ export default {
       ssl: true,
     },
   },
-  test: {},
   production: {
     dialect: 'postgres',
     host: process.env.DB_HOST,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-
-    // url: process.env.DB_URL,
-    // dialectOptions: {
-    //   ssl: true,
-    // },
   },
 };
