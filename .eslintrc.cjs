bf60de9a241167a0b0f1830bf0139de3ed496b6f
Jason Barry <jbarry@hey.com>
2024-12-15 21:46:24 -0500

    update linting

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    // requireConfigFile: false,
+  },
+  plugins: ['prettier'],
+  extends: ['eslint:recommended', 'plugin:n/recommended', 'plugin:prettier/recommended'], //'plugin:prettier/recommended',
+  env: {
+    browser: false,
+    node: true,
+  },
+  rules: {},
+  overrides: [],
+};
