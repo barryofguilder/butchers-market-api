8d37f8dcc2b4506465ab68c9db17136202db26d4
Jason Barry <jbarry@hey.com>
2024-12-17 20:53:18 -0500

    Roll back typescript to fix Render

--- src/utilities/is-blank.js
+++ src/utilities/is-blank.js
@@ -4,6 +4,6 @@
  * @param field The field to check if it is blank.
  * @returns Returns true if the field is blank, otherwise false.
  */
-export function isBlank(field: string | null | undefined) {
+export function isBlank(field) {
   return field === undefined || field === null || field.trim() === '';
 }
